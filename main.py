28bcc2edd74b5c40d63ad2eff19923ef1cd86c67
ezekielkoh <ezekiel.kohck@gmail.com>
2024-08-02 09:59:01 +0800

    enabled logging function

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from utils.io import (fetch_data,
                       format_resale_df)
 from utils.io import read_config, save_data
 
+import logging
 
 
 if __name__ == '__main__':
+    log_file = 'logs/pipeline.log'
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler(log_file)])
+    
+    logger = logging.getLogger(__name__)
+    logger.info('Running script')
     rental_config = read_config(key='rental')
     resale_config = read_config(key='resale')
 
     resale_df = fetch_data(**resale_config, refresh_data='resale.parquet')
     rental_df = fetch_data(**rental_config, refresh_data='rental.parquet')
     save_data(resale_df, 'test')
-    print('works')
+    logger.info('Script completed')
