1ffd68ead3312c2a2f2ca8f7d7c4006fddb184a6
ezekielkoh <ezekiel.kohck@gmail.com>
2024-08-02 09:53:49 +0800

    added save data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils.io import (fetch_data,
                       read_config,
                       format_rental_df, 
                       format_resale_df)
-from utils.io import read_config   
+from utils.io import read_config, save_data
 
 
 
@@ -12,4 +12,5 @@ if __name__ == '__main__':
 
     resale_df = fetch_data(**resale_config, refresh_data='resale.parquet')
     rental_df = fetch_data(**rental_config, refresh_data='rental.parquet')
+    save_data(resale_df, 'test')
     print('works')
