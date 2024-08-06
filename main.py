186eea2e8f084c5cac3494665dd8a7c73821b831
ezekielkoh <ezekiel.kohck@gmail.com>
2024-08-06 16:45:49 +0800

    updated logger

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from utils.io import read_config, save_data, config_logger
 if __name__ == '__main__':
     logger = config_logger('pipeline', file_path='logs/pipeline.log')
     logger.info('Running script')
+    logger.info('Reading rental config file')
     rental_config = read_config(key='rental')
+    logger.info('Reading resale config file')
     resale_config = read_config(key='resale')
 
+    logger.info('Fetching resale data')
     resale_df = fetch_data(**resale_config, refresh_data='resale')
+    logger.info('Fetching rental data')
     rental_df = fetch_data(**rental_config, refresh_data='rental')
     logger.info('Script completed')
